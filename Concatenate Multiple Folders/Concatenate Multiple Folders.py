fca65096d8bbcee20320ef29ac149ea335ae7267
Fabcooldog <fabcooldog@Zoobuh.com>
2016-08-26 12:59:13 +0100

    Bug fixes and start of changing metadata

--- Concatenate Multiple Folders/Concatenate Multiple Folders.py
+++ Concatenate Multiple Folders/Concatenate Multiple Folders.py
@@ -1,10 +1,10 @@
 # Author= Jacob Wilson
 # Date= 23/09/16
 
-from PyPDF2 import PdfFileMerger
+import PyPDF2
 from os import listdir, path
 from time import time
-
+from warnings import warn
 starttime = time()
 
 # Edit this variable to suit your needs
@@ -13,24 +13,29 @@ folder_path = r'C:\Users\Jacob\Documents\GitHub\PDF-Manipulation\Concatenate Mul
 
 
 def concatenate_pdfs(file_list, output_folder_path, output_filename):
-    assert not path.exists(r'{0}\{1}.pdf'.format(output_folder_path, output_filename)), \
-        'There is already a pdf of name "{0}" at the location "{1}"'\
-            .format(output_filename, r'{0}\{1}.pdf'.format(output_folder_path, output_filename))
-    new_merger = PdfFileMerger()
-    for file in file_list:
-        new_merger.append(r'{0}'.format(file))
-
+    if path.exists(r'{0}\{1}.pdf'.format(output_folder_path, output_filename)):
+        warn('There is already a pdf of name "{0}" at the location "{1}"'.format(output_filename, r'{0}\{1}.pdf'.format(
+            output_folder_path, output_filename)))
+        return
+    new_merger = PyPDF2.PdfFileMerger(strict=False)
+    for pdf in file_list:
+        new_merger.append(r'{0}'.format(pdf))
+    new_merger.addMetadata({u'/Title': u'My title', u'/Version': u'PDF-1.5'
+                            })
+    #print(new_merger.pages[0].__dict__)
+    #new_merger.header.replace(PyPDF2.utils.b_("PDF-1.3"),PyPDF2.utils.b_("PDF-1.5"))
+    #PyPDF2.PdfFileWriter()
     new_merger.write(r'{0}\{1}.pdf'.format(output_folder_path, output_filename))
 
 
-files = []
 for folder in listdir(r'{0}'.format(folder_path)):
-    # print(folder)
     if path.isdir(r'{0}\{1}'.format(folder_path, folder)):
+        files = []
         for file in listdir(r'{0}\{1}'.format(folder_path, folder)):
             if file.lower().endswith('.pdf'):
                 files.append(r'{0}\{1}\{2}'.format(folder_path, folder, file))
-                files.sort()
+
+        files.sort()
         concatenate_pdfs(files, r'{0}\{1}'.format(folder_path, folder), folder)
 
 endtime = time()
